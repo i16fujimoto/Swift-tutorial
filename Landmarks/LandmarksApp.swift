1d58512c7489cb5d904ae0e59979126b28eb3782
i16fujimoto <fujimoto.ryusei@arakawa-lab.com>
2022-12-01 17:46:08 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ryusei Fujimoto on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
